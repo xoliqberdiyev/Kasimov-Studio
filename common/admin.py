700a6cc7545ed9cb1a7a33e28bc0452962a4611a
behruz-dev <xoliqbediyevbehruz@gmail.com>
2025-06-10 13:28:37 +0500

    change

--- common/admin.py
+++ common/admin.py
@@ -5,10 +5,10 @@ from modeltranslation.admin import TranslationAdmin
 from common import models 
 
 
-admin.site.register(models.ServiceCategory, TranslationAdmin)
-admin.site.register(models.Service, TranslationAdmin)
+admin.site.register(models.ServiceCategory)
+admin.site.register(models.Service)
 admin.site.register(models.Gallery)
 admin.site.register(models.Partner)
-admin.site.register(models.AboutUs, TranslationAdmin)
+admin.site.register(models.AboutUs)
 admin.site.register(models.Video)
-admin.site.register(models.Team, TranslationAdmin)
+admin.site.register(models.Team)
